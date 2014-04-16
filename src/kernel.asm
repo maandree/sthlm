a3968af23eaee51ecc0faf4a7b286daa1e21fa57
Mattias Andrée <maandree@operamail.com>
2014-04-16 21:10:26 +0200

    nicer assembly code
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,14 +1,25 @@
 bits 32					; 32-bits
 
+	
 section .text
 	; Multiboot specification: http://www.gnu.org/software/grub/manual/multiboot
 	align 4
-	dd 0x1BADB002			; magic number
-	dd 0x00				; flags
-	dd - (0x1BADB002 + 0x00)	; checksum
-
+	
+	MULTIBOOT_HEADER_MAGIC	equ 0x1BADB002
+	MULTIBOOT_HEADER_FLAGS	equ 0
+	MULTIBOOT_CHECKSUM	equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+	
+	dd MULTIBOOT_HEADER_MAGIC
+	dd MULTIBOOT_HEADER_FLAGS
+	dd MULTIBOOT_CHECKSUM
+	
+	
+	; Global functions
 	global start
-	extern kmain			; kmain is defined elsewhere
+	
+	; Functions defined in C code
+	extern kmain
+
 
 start:
 	cli				; block interrupts
